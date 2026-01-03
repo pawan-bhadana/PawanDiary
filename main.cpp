05c8861b4e7eded0b4f412e251eae868f7fb7757
Pawan Bhadana <2002pawanbhadana@gmail.com>
2026-01-03 12:42:07 +0530

    addNote function implemented

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 #include <string>
 
 using namespace std;
+vector<string> Diary;
+
+int addNote(){
+    string entry;
+    cout<<"Start making entry in the next line";
+    getline(cin,entry);
+    Diary.push_back(entry);
+    return Diary.size();
+}
 
 int main(){
-
+    addNote();
     return 0;
 }
